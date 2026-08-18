898d50df59b516ac52583dcf4e9a238f82aac905
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2026-08-18 11:25:36 +0530

    Add email and website information to hello.py

--- hello.py
+++ hello.py
@@ -8,3 +8,8 @@ print("""Your Learning Path:" \
 \t - Python Basics 
 \t Data Engineering 
 \t AI""")
+
+name="datawithharmeet.com"
+print("info@", name)
+print("support@", name)
+print("www@", name)
