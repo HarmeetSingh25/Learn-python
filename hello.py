56dc0b95dcee12d786c7e8da39a4dec3e06ed73c
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2026-08-18 11:20:35 +0530

    Add initial implementation of learning path display in hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+# print(" hello ")
+# print("hello \"python\"")
+# print("path: c:\\harmeet\\singh")
+
+# print("Your Learning Path:\n\t - Python Basics \n\t Data Engineering \n\t AI")
+
+print("""Your Learning Path:" \
+\t - Python Basics 
+\t Data Engineering 
+\t AI""")
