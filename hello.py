489b7e6fdfb70cef9921dc337c14f4965e040716
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2026-08-19 19:17:59 +0530

    Refactor code by commenting out unused print statements and organizing variable declarations

--- hello.py
+++ hello.py
@@ -4,12 +4,25 @@
 
 # print("Your Learning Path:\n\t - Python Basics \n\t Data Engineering \n\t AI")
 
-print("""Your Learning Path:" \
-\t - Python Basics 
-\t Data Engineering 
-\t AI""")
+# print("""Your Learning Path:" \
+# \t - Python Basics
+# \t Data Engineering
+# \t AI""")
 
-name="datawithharmeet.com"
-print("info@", name)
-print("support@", name)
-print("www@", name)
+# name="datawithharmeet.com"
+# print("info@", name)
+# print("support@", name)
+# print("www@", name)
+
+age = 22
+height = 5.5
+name = "Harmeet"
+isStudent = True
+salary = None
+print("Name:", name, "NameLength:", len(name),"Name:",type(name),
+       "\n", "Height:", height,"Height:", type(height),
+       "\n", "Age", age, "Age", age.bit_length(),"Age", type(age),
+      "\n", "IsStudent", isStudent ,"\n", "IsStudent", isStudent.bit_length(),"IsStudent" , type(isStudent),
+        "\n","Salary", salary, "Salary", type(salary) )
+
+print(name.count)
